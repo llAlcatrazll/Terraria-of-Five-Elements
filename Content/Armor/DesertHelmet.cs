964c04eb0030c1e1aff7068e31a1beedb01cbfa3
Edmund Nillas III <edmundnillas3rd@gmail.com>
2024-06-25 22:27:42 +0800

    Add new vanity set

---
+++ Content/Armor/DesertHelmet.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace TFE.Content.Armor
+{
+    [AutoloadEquip(EquipType.Head)]
+    public class DesertHelmet : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.defense = 10;
+            Item.rare = ItemRarityID.Expert;
+        }
+
+        public override void UpdateEquip(Player player)
+        {
+
+        }
+
+        public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe();
+            recipe.AddIngredient(ItemID.Wood, 10);
+            recipe.Register();
+        }
+    }
+}
